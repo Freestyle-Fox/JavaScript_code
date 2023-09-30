760124eff2903647983ecf33ec39117b8116b196
Daark.8 <79575487+Freestyle-Fox@users.noreply.github.com>
2023-09-30 12:41:06 +0530

    Create printMonthOfTheQuarter.js
    months of the quarter

---
+++ printMonthOfTheQuarter.js
@@ -0,0 +1,17 @@
+let Quarter=prompt("Enter Quater number to know months of the quater (1,2,3,4) : ")
+switch(Quarter){
+    case "1":
+        console.log("January , February , March");
+        break;
+    case "2":
+        console.log(" April, May , June");
+        break;
+    case "3":
+        console.log("July , August , September");
+        break;
+    case "4":
+        console.log("October , November , December");
+        break;
+    default:
+        console.log("Please enter from this (1,2,3,4) ")
+}
