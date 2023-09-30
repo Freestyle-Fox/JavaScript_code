bcced07cf1e90b49cb169de4c153b479ea8831c9
Daark.8 <79575487+Freestyle-Fox@users.noreply.github.com>
2023-09-30 12:01:13 +0530

    Create welocmeMessageWithAlert.js
    using template literals print msg in with alert

---
+++ welocmeMessageWithAlert.js
@@ -0,0 +1,5 @@
+//WAP to concatinate your first name and last name and print message with the help of alert using template literals
+let f=prompt("Enter your first name : ")
+let l=prompt("Enter your last name : ")
+// console.log(`welcome here, ${f+" "+l}`)    // this line will print the message in console 
+alert(`welcome here, ${f+" "+l}`)               // this line will print the message in with alert 
