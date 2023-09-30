9bfe302c81728cab69d8696266490908404d297c
Daark.8 <79575487+Freestyle-Fox@users.noreply.github.com>
2023-09-30 11:46:36 +0530

    Update largestOfThreeNumber.js

--- largestOfThreeNumber.js
+++ largestOfThreeNumber.js
@@ -1,7 +1,11 @@
+// A program to find largest of 3 number 
 let num1=prompt("Enter the fisrt number : ")
 let num2=prompt("Enter the second number : ")
-if (num1[num1.length-1]==num2[num2.length-1]){
-    console.log("Last digit of this number is equal")
-}else{
-    console.log("Not equal")
+let num3=prompt("Enter the third number : ")
+if(num1 > num2 && num1 >num3){
+    console.log(`largest of three number is : ${num1} `)
+}else if(num2 > num1 && num2 >num3){
+    console.log(`largest of three number is : ${num2} `)
+}else{ 
+    console.log(`largest of three number is : ${num3} `)
 }
