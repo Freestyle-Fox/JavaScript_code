b47a25abd02bbcce374e73de7db169ad4bb72f38
Daark.8 <79575487+Freestyle-Fox@users.noreply.github.com>
2023-09-30 11:45:30 +0530

    Create largestOfThreeNumber.js

---
+++ largestOfThreeNumber.js
@@ -0,0 +1,7 @@
+let num1=prompt("Enter the fisrt number : ")
+let num2=prompt("Enter the second number : ")
+if (num1[num1.length-1]==num2[num2.length-1]){
+    console.log("Last digit of this number is equal")
+}else{
+    console.log("Not equal")
+}
